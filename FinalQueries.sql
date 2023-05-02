75a0a83c8e4bc1ba78180ef68e727c6f92b1d914
BrandonPanuccio <131029555+BrandonPanuccio@users.noreply.github.com>
2023-05-02 12:01:07 -0400

    Add files via upload

---
+++ FinalQueries.sql
@@ -0,0 +1,15 @@
+select COUNT (*) from matches;
+select COUNT (*) from coach;
+select COUNT (*) from player;
+select COUNT (*) from accolades;
+select COUNT (*) from country;
+
+select * from matches;
+select * from coach;
+select * from player;
+select * from accolades;
+select * from country;
+
+select * 
+from player
+where country_name = 'Argentina'; 
