addcfddb8d8d9bd8661b5a2b73ad00ad758583fa
chume2 <119360496+chume2@users.noreply.github.com>
2023-05-02 01:29:16 -0400

    Update FinalProjectSchema.sql

--- FinalProjectSchema.sql
+++ FinalProjectSchema.sql
@@ -13,7 +13,7 @@ CREATE TABLE Country (
   name VARCHAR(50) PRIMARY KEY,
   continent VARCHAR(50) NOT NULL,
   group_letter VARCHAR(1) NOT NULL,
-  number_of_goals INT NOT NULL DEFAULT 0,
+  number_of_goals INT NOT NULL,
   placing VARCHAR(50) NOT NULL
 );
 
